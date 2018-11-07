922fbdc1e10aa81d2dd5abd60faa697dd544cb1a
saratabitha.mayhew <saratabitha.mayhew@gmail.com>
2018-11-07 16:54:55 -0600

    nav bar

--- PHP/header.php
+++ PHP/header.php
@@ -3,5 +3,12 @@
             <h1 id="way_morr" class="sport_font">WAY-MORR</h1>
             <h2 id="youth_sports" class="sport_font">YOUTH SPORTS</h2>
             </div>
+
+            <nav>
+                <ul >
+                    <li><a class="sport_font">Home</a></li>
+                    <li><a class="sport_font">Test</a></li>
+                </ul>
+            </nav>
 </header>
 
