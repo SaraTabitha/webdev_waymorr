15f2bd515c6d394b5f2538649e7504b98cb53c6f
saratabitha.mayhew <saratabitha.mayhew@gmail.com>
2018-11-07 16:25:56 -0600

    header.php made and linked to home.php
    homepage renamed to home.php instead of waymorr.html; php pages made for header & footer

---
+++ PHP/header.php
@@ -0,0 +1,7 @@
+<header>
+            <div id="logo">
+            <h1 id="way_morr" class="sport_font">WAY-MORR</h1>
+            <h2 id="youth_sports" class="sport_font">YOUTH SPORTS</h2>
+            </div>
+</header>
+
--- home.php
+++ home.php
@@ -12,11 +12,8 @@
         <link href="https://fonts.googleapis.com/css?family=Rubik+Mono+One" rel="stylesheet">    
     </head>
     <body>
-        <header>
-            <div id="logo">
-            <h1 id="way_morr" class="sport_font">WAY-MORR</h1>
-            <h2 id="youth_sports" class="sport_font">YOUTH SPORTS</h2>
-            </div>
-        </header>
+        <?php include "PHP/header.php" ?>
+
+        <?php include "PHP/footer.php" ?>
     </body>
 </html>
