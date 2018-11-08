47458c6ec83bd7f36987cf4c8c8bdd1a1af54a13
saratabitha.mayhew <saratabitha.mayhew@gmail.com>
2018-11-07 22:31:32 -0600

    about us update; nav submenu white background fix

--- site/about_us.php
+++ site/about_us.php
@@ -3,7 +3,29 @@
     <?php include_once 'PHP/head.php' ?>
     <body>
         <?php include_once "PHP/header.php" ?>
-        <p>about us</p>
+        <h1>About Us</h1>
+        <h2>Our Mission</h2>
+        <p>Give every child in the Wayside/Morrison area an opportunity to play ball.</p>
+        <h2>Contact Information</h2>
+        <p>
+            <a href="mailto:waymorryouthsports@gmail.com">waymorryouthsports@gmail.com</a>
+        </p>
+        <address>
+            8123 Dickinson Road </br>
+            Greenleaf, WI, 54126</br>
+        </address>
+        </br>
+        <p><strong>President:</strong><p>
+            <p>Alec Otto</p>
+            <address>
+                7578 Dickinson Road </br>
+                Greenleaf, WI, 54126 </br>
+            </address>
+            <p>
+                <a href="mailto:alecotto75@gmail.com">alecotto75@gmail.com</a>
+            </p>
+            <a href="tel:9203285374">(920)328-5374</a>
+            </br>
         <?php include_once "PHP/footer.php" ?>
     </body>
 </html>
