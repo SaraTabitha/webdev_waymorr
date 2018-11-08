c30ce3e57b70d6086b7a6c421c9dae800a7b2f07
saratabitha.mayhew <saratabitha.mayhew@gmail.com>
2018-11-07 20:54:43 -0600

    footer styling

--- PHP/footer.php
+++ PHP/footer.php
@@ -1,5 +1,14 @@
 <footer>
-    
+    <div>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        
+    </div>
+
+
+
     <script src="jQuery/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="JS/functions.js"> </script>
 </footer>
