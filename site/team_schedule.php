78ed2533b8f60799b96af5ae7c5bc150c96dc1b6
mayhes06 <mayhes06@UWO5349W0>
2018-12-08 12:23:30 -0600

    team schedule page -> if not logged in redirects to home; if logged in, checks if user is an coach (coach view) or user/admin (user view)

--- site/team_schedule.php
+++ site/team_schedule.php
@@ -3,8 +3,31 @@
     <?php include_once 'PHP/head.php' ?>
     <body>
         <?php include_once "PHP/header.php" ?>
-        <h1>Team Schedule</h2>
-        <p>coach only!</p>
+
+		<?php
+			if(isset($_SESSION['user_id'])){
+			?>
+				<h1>Team Schedule</h2>
+			<?php
+				if(isset($_SESSION['isCoach']) && ($_SESSION['isCoach'] === true)){
+				//coach
+				?>
+					 <p>coach view</p>
+				<?php
+				}
+				else{
+				//logged in user/admin
+				?>
+					 <p>user/admin view</p>
+				<?php
+				
+				}
+			}
+			else{
+				redirect("home.php");
+			}
+		?>
+        
         <?php include_once "PHP/footer.php" ?>
     </body>
 </html>
