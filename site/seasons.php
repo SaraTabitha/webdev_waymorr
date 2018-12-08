feac74f5b216fcd4e4b053a426f50d4f0a48ab55
mayhes06 <mayhes06@UWO5349W0>
2018-12-08 16:01:54 -0600

    settings dropdown & new 3 pages/links

---
+++ site/seasons.php
@@ -0,0 +1,9 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+    <?php include_once 'PHP/head.php' ?>
+<body>
+    <?php include_once "PHP/header.php" ?>
+    <h1>Seasons </h1>
+    <?php include_once "PHP/footer.php" ?>
+</body>
+</html>
