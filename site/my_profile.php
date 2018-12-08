a2e8f926fa49dcc194bb132ae161c011c804b631
saratabitha.mayhew <saratabitha.mayhew@gmail.com>
2018-12-07 18:55:48 -0600

    profile if/else logged in
    checks if user_id is set (user is logged in)
    + needs user_id to access info from db

--- site/my_profile.php
+++ site/my_profile.php
@@ -4,16 +4,20 @@
     <body>
         <?php include_once "PHP/header.php" ?>
         <?php 
-        if(){
-            
+        if(isset($_SESSION['user_id'])){
+            ?>
+                <h1>My Profile</h1>
+                <p>Firstname, lastname</p>
+                <p>Registered for... teams</p>
+                <h2>Contact Info</h2>
+                <p>email@email.com</p> <button>edit email</button>
+            <?php
         }
-        
+        else{
+            redirect("login.php");
+        }
         ?>
-        <h1>My Profile</h1>
-        <p>Firstname, lastname</p>
-        <p>Registered for... teams</p>
-        <h2>Contact Info</h2>
-        <p>email@email.com</p> <button>edit email</button>
+        
         <?php include_once "PHP/footer.php" ?>
     </body>
 </html>
